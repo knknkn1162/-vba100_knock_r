c066eba1ec391f1e283f3759eccafcb079ad4e49
Kenta_Nakajima <knknkn1162@gmail.com>
2023-09-18 10:47:03 +0900

    use group_walk

--- src/ex027/main01.R
+++ src/ex027/main01.R
@@ -1,3 +1,3 @@
 read_excel("../ExcelVBAPractice.xls", sheet="練習27") |>
   group_by(支店) |>
-  group_map(\(x,y) write_excel_csv(x, str_glue("{y}.csv")))
+  group_walk(\(x,y) write_excel_csv(x, str_glue("{y}.csv")))
