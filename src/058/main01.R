a3a2138b34f8b1f998d9035e29fd4e0e20917195
Kenta_Nakajima <knknkn1162@gmail.com>
2023-09-18 11:30:40 +0900

    use full_seq in 058

--- src/058/main01.R
+++ src/058/main01.R
@@ -1,7 +1,8 @@
 n <- readline() |> parse_number()
-1:22 |>
+vec <- c(1,2,3,5,8,9,11,12,13,14,15,17,19,20,21,22)
+vec |> full_seq(period = 1) |>
   as_tibble() |>
-  mutate(flag = value %in% c(1,2,3,5,8,9,11,12,13,14,15,17,19,20,21,22) ) |>
+  mutate(flag = value %in% vec ) |>
   mutate(v = consecutive_id(flag)) |>
   filter(flag) |>
   group_by(v) |>
